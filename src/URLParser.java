ecf449d0491be29ffd8b9ebd6645f129ed436122
mdailerian <martin@dailerian.com>
2016-05-01 15:27:37 -0400

    Initial commit

---
+++ src/URLParser.java
@@ -0,0 +1,11 @@
+/**
+ * Created by MMAA-local on 5/1/2016.
+ */
+public class URLParser {
+    public URLParser( String url){
+
+    }
+    public int dummy(){
+        return i;
+    }
+}
---
+++ src/URLParserTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MMAA-local on 5/1/2016.
+ */
+public class URLParserTest {
+
+    @Test
+    public void dummy_basic_http_protocol(){
+    URLParser p = new URLParser("http://www.google.com");
+    //assertEquals()
+    }
+}
