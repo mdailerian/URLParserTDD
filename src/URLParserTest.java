64956172b75f7d3ec293dd4ee920de5cfac90b03
mdailerian <martin@dailerian.com>
2016-05-01 15:28:43 -0400

    add links

--- src/URLParserTest.java
+++ src/URLParserTest.java
@@ -2,6 +2,19 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/*
+TDD
+        No production code without a failing test
+        4 steps
+        Start with the test
+        Write production code
+        refactor test
+        refactor production code
+        BDD
+        https://vimeo.com/10569751
+        https://www.youtube.com/watch?v=hD9e6AauOh0
+        https://www.youtube.com/watch?v=xubiP8WoT4E&index=2&list=PLBBog2r6uMCSa9_mNISSpTcbTcKlMAe6t
+*/
 /**
  * Created by MMAA-local on 5/1/2016.
  */
